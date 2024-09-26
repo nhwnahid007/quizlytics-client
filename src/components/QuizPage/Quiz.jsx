47206e1a4f9f074530c6e940b120aa776f66c4dc
rafiul-razib <rafiul.rzb1@gmail.com>
2024-09-26 15:48:43 +0600

    design updated

--- src/components/QuizPage/Quiz.jsx
+++ src/components/QuizPage/Quiz.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { flushSync } from "react-dom";
 import { GrRadialSelected } from "react-icons/gr";
 
 const Quiz = ({ question, currentQuestion, totalQuestion, setAnswer }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const timer = useRef(null);
+  const progressBar = useRef(null);
+
+  const handleGoToNextQuiz = () => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+
+    flushSync(() => {
+      setAnswer(selectedOption);
+    });
+
+    setSelectedOption(null);
+  };
+
+  useEffect(() => {
+    timer.current = setTimeout(handleGoToNextQuiz, 100000);
+  }, [question]);
   return (
     <div className="max-w-3xl p-20 shadow-2xl mx-auto my-12">
       <div>
@@ -10,18 +30,22 @@ const Quiz = ({ question, currentQuestion, totalQuestion, setAnswer }) => {
         </h1>
         <div>
           <h1 className="text-start font-semibold text-lg text-red-600 mb-4">
-            Question: {question.question}
+            <span className="text-warning">Question : </span>
+            {question.question}
           </h1>
         </div>
-        <div>
-          <ol>
-            {question.options.map((item) => (
-              <li key={item.id} className="flex justify-start items-center">
-                <GrRadialSelected className="mx-2" />
-                {item}
-              </li>
-            ))}
-          </ol>
+        <div className="h-32">
+          {question.options.map((item) => (
+            <div key={item.id} className="flex gap-2 items-center">
+              <GrRadialSelected className="mx-2" />
+              {item}
+            </div>
+          ))}
+        </div>
+        <div className="my-2">
+          <button className="btn btn-warning" onClick={handleGoToNextQuiz}>
+            Next
+          </button>
         </div>
       </div>
     </div>
