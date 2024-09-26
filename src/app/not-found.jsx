1e1cbdae5d7ff2c0b5cccee91029fab72af5c934
msahrana <msahrana@gmail.com>
2024-09-27 00:45:17 +0600

    not found page added

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,21 +3,30 @@ import React from "react";
 
 const notFound = () => {
   return (
-    <div>
-      <div className="absolute mx-auto">
-        <h1 className="text-9xl font-extrabold">404</h1>
-        <h2 className="text-4xl font-semibold">
+    <div
+      className="h-[100vh] bg-cover bg-right bg-no-repeat text-white"
+      style={{
+        backgroundImage: `url('https://i.ibb.co.com/X4fQqDn/question-mark-1872634-1920.jpg')`,
+      }}
+    >
+      <div className="mx-auto space-y-4 pt-64">
+        <h1 className="text-9xl font-extrabold text-center text-[#ff0000]">
+          404
+        </h1>
+        <h2 className="text-4xl font-semibold text-center">
           Sorry, we couldn`t find this page.
         </h2>
-        <h3 className="text-xl">
+        <h3 className="text-xl text-center">
           But don`t worry, you can find plenty of other things on our homepage
         </h3>
-        <Link
-          href={"/"}
-          className="bg-red-500 px-4 py-2 rounded-md text-3xl text-white"
-        >
-          Go Home
-        </Link>
+        <div className="mx-auto text-center justify-center items-center pt-5">
+          <Link
+            href={"/"}
+            className="bg-[#ff0000] hover:bg-[#ffefd3] px-4 py-2 rounded-md text-white text-3xl"
+          >
+            Go Home
+          </Link>
+        </div>
       </div>
     </div>
   );
