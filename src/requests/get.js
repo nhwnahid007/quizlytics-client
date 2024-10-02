1e0a70a58587b9150ec8567784ed73d1e6998ec7
tanvir244 <tr30365s25@gmail.com>
2024-10-02 18:35:32 +0600

    server deploy link inserted

--- src/requests/get.js
+++ src/requests/get.js
@@ -13,25 +13,10 @@ export const getMCQ = async (category, level) => {
 
 export const getMark = async(examId) => {
     try {
-        const res = await axios.get(`http://localhost:5000/my_mark/${examId}`);
+        const res = await axios.get(`https://quizlytics-server-gamma.vercel.app/my_mark/${examId}`);
         return res.data;
     } catch (error) {
         console.error("Error fetching MCQ:", error);
         return [];
     }
 }
-
-
-// export const getMCQ = async (category, level) => {
-//     console.log(category, level);
-//     try { 
-//         const res = await axios.get('/data.json'); // Fetch from public folder
-//         return res.data;
-//     } catch (error) {
-//         console.error("Error fetching MCQ:", error);
-//         return [];
-//     }
-// }
-
-// https://quizlytics.jonomukti.org/quiz?category=nodejs&skill=beginner
-// https://quiz.muktikathan.com/quiz?category=html&skill=advanced
