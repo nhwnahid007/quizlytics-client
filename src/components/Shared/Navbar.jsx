32677e0d638355091c0b7ce12701d311d792ba26
nhwnahid007 <nhwnahid@gmail.com>
2024-10-10 21:25:24 +0600

    added blogs

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,9 +1,9 @@
 "use client"; // Ensure this is the first line in the file
 
-import { signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from "next-auth/react";
 import React, { useEffect, useRef, useState } from "react";
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   const dropdownRef = useRef(null);
@@ -33,7 +33,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className={`py-2 ${isActive ? 'bg-gray-800' : 'bg-black'} text-white`}>
+    <div className={`py-2 ${isActive ? "bg-gray-800" : "bg-black"} text-white`}>
       <div className="navbar max-w-6xl mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
@@ -57,56 +57,76 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li><Link href='/'>Home</Link></li>
               <li>
-                <a>Category</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
+                <Link href="/">Home</Link>
               </li>
-              <li><Link href='/about'>About</Link></li>
-              <li><Link href='/contact'>Contact</Link></li>
-              <li><Link href='/team'>Our Team</Link></li>
+              <li>
+                <Link href="blogs">Blogs</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link href="/team">Our Team</Link>
+              </li>
             </ul>
           </div>
           <Link href="/" className="text-4xl text-[#ff0000] font-bold">
-            Quiz<span className='text-[#ffefd3]'>lytics</span>
+            Quiz<span className="text-[#ffefd3]">lytics</span>
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex relative z-10 text-[#ffefd3]">
           <ul className="menu menu-horizontal px-1 text-base">
-            <li><Link href='/'>Home</Link></li>
             <li>
-              <details ref={dropdownRef} onClick={handleDropdownClick}>
-                <summary>Category</summary>
-                <ul className="p-2 text-black">
-                  <li><Link href='/'>Submenu 1</Link></li>
-                  <li><Link href='/'>Submenu 2</Link></li>
-                </ul>
-              </details>
+              <Link href="/">Home</Link>
             </li>
-            <li><Link href='/about'>About</Link></li>
-            <li><Link href='/contact'>Contact</Link></li>
-            <li><Link href='/team'>Our Team</Link></li>
+            <li>
+              <summary>
+                <Link href="blogs">Blogs</Link>
+              </summary>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/team">Our Team</Link>
+            </li>
           </ul>
         </div>
         <div className="navbar-end flex gap-4">
           {!session ? (
-            <div className='flex gap-2 md:gap-4'>
-              <Link href='/login' className="btn px-2 md:px-8 bg-[#ffefd3]">Login</Link>
-              <Link href='/register' className="btn px-2 md:px-8 bg-[#ffefd3]">Register</Link>
+            <div className="flex gap-2 md:gap-4">
+              <Link href="/login" className="btn px-2 md:px-8 bg-[#ffefd3]">
+                Login
+              </Link>
+              <Link href="/register" className="btn px-2 md:px-8 bg-[#ffefd3]">
+                Register
+              </Link>
             </div>
           ) : (
             <div className="dropdown dropdown-end">
-              <div tabIndex={0} role="button" className="btn btn-ghost w-[64px] h-[64px] avatar">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost w-[64px] h-[64px] avatar"
+              >
                 <div className="w-full h-full rounded-full">
                   <Image
-                    src={profile || image || 'https://i.ibb.co.com/ts4kH5c/istockphoto-1337144146-612x612.jpg'}
-                    alt='user'
+                    src={
+                      profile ||
+                      image ||
+                      "https://i.ibb.co.com/ts4kH5c/istockphoto-1337144146-612x612.jpg"
+                    }
+                    alt="user"
                     width={64} // Explicitly set the width
                     height={64} // Explicitly set the height
-                    className='rounded-full w-full h-full'
+                    className="rounded-full w-full h-full"
                   />
                 </div>
               </div>
@@ -114,12 +134,14 @@ const Navbar = () => {
                 tabIndex={0}
                 className="menu menu-sm dropdown-content bg-[#ffefd3] rounded-box mt-3 w-60 p-6 relative z-20 shadow"
               >
-                <li className='text-center text-black font-bold text-lg px-4 mb-4 leading-6'>{name}</li>
-                <li className='bg-black text-[#ffefd3] p-2 rounded-xl mb-2'>
-                  <Link href='/Dashboard'>My Dashboard</Link>
+                <li className="text-center text-black font-bold text-lg px-4 mb-4 leading-6">
+                  {name}
                 </li>
-                <li 
-                  className='bg-black text-[#ffefd3] px-4 py-2 rounded-xl cursor-pointer' 
+                <li className="bg-black text-[#ffefd3] p-2 rounded-xl mb-2">
+                  <Link href="/Dashboard">My Dashboard</Link>
+                </li>
+                <li
+                  className="bg-black text-[#ffefd3] px-4 py-2 rounded-xl cursor-pointer"
                   onClick={() => signOut()}
                 >
                   Logout
