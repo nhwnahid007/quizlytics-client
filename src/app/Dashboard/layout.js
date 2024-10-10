fd5183d8f40e5d562d926e1fc0f40cfec3cbf9c1
musaakramsaleh <saleh01715972188@gmail.com>
2024-10-10 09:32:19 +0600

    ok

--- src/app/Dashboard/layout.js
+++ src/app/Dashboard/layout.js
@@ -1,16 +1,20 @@
-import Navbar from "@/components/Shared/Navbar";
 import "../globals.css";
 import Footer from "@/components/Shared/Footer";
 import AuthProviders from "@/providers/AuthProviders";
+import Sidebar from "@/components/Shared/Sidebar";
 
 export default function RootLayout({ children }) {
     return (
-        <div>
-            <AuthProviders>
-                <div>
+            <div className="flex min-h-screen">
+              <Sidebar />
+              <div className="flex-grow">
+                <AuthProviders>
+                  <div>
                     {children}
-                </div>
-            </AuthProviders>
-        </div>
+                  </div>
+                </AuthProviders>
+              </div>
+            </div>
+         
     )
 }
