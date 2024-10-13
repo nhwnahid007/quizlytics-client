2ea1501832a6b83987982a4fd566d2cebd6c1d00
rafiul-razib <rafiul.rzb1@gmail.com>
2024-10-13 19:31:09 +0600

    examiners dashboard updated

--- src/app/Dashboard/(routes)/examinersDashboard/page.jsx
+++ src/app/Dashboard/(routes)/examinersDashboard/page.jsx
@@ -1,6 +1,16 @@
 "use client";
 import { allCustomQuiz } from "@/requests/get";
 import { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   const [allQuiz, setAllQuiz] = useState([]);
@@ -23,30 +33,71 @@ const Page = () => {
   }, []);
 
   return (
+    // <div>
+    //   <div className="overflow-x-auto">
+    //     <table className="table">
+    //       {/* head */}
+    //       <thead>
+    //         <tr>
+    //           <th className="text-center"></th>
+    //           <th className="text-center">Quiz Title</th>
+    //           <th className="text-center">Quiz Key</th>
+    //           <th className="text-center">Number of Question</th>
+    //         </tr>
+    //       </thead>
+    //       <tbody>
+    //         {allQuiz.map((item, idx) => (
+    //           <tr key={item.quizStartKey} className="bg-base-200">
+    //             <th className="text-center">{idx + 1}</th>
+    //             <td className="text-center">{item.quizTitle}</td>
+    //             <td className="text-center">{item.quizStartKey}</td>
+    //             <td className="text-center">{item.quizArr.length}</td>
+    //           </tr>
+    //         ))}
+    //       </tbody>
+    //     </table>
+    //   </div>
+    // </div>
     <div>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th className="text-center"></th>
-              <th className="text-center">Quiz Title</th>
-              <th className="text-center">Quiz Key</th>
-              <th className="text-center">Number of Question</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allQuiz.map((item, idx) => (
-              <tr key={item.quizStartKey} className="bg-base-200">
-                <th className="text-center">{idx + 1}</th>
-                <td className="text-center">{item.quizTitle}</td>
-                <td className="text-center">{item.quizStartKey}</td>
-                <td className="text-center">{item.quizArr.length}</td>
-              </tr>
+      <main className="max-w-6xl mx-auto my-12">
+        <Table>
+          <TableCaption>ALL CUSTOM QUESTIONS</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Quiz Code</TableHead>
+              <TableHead>Title</TableHead>
+              <TableHead>View Quiz</TableHead>
+              <TableHead>Quiz Creator</TableHead>
+              <TableHead className="text-center">Number of Qusetions</TableHead>
+              <TableHead>Update Quiz</TableHead>
+              <TableHead className="text-right">Delete Quiz</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {allQuiz.map((item) => (
+              <TableRow key={item.quizStartKey}>
+                <TableCell className="font-medium">
+                  {item.quizStartKey}
+                </TableCell>
+                <TableCell>{item.quizTitle}</TableCell>
+                <TableCell>
+                  <Button variant="outline">View</Button>
+                </TableCell>
+                <TableCell>{item.creatorEmail}</TableCell>
+                <TableCell className="text-center">
+                  {item.quizArr.length}
+                </TableCell>
+                <TableCell>
+                  <Button variant="secondary">Update</Button>
+                </TableCell>
+                <TableCell className="text-right">
+                  <Button variant="destructive">Delete</Button>
+                </TableCell>
+              </TableRow>
             ))}
-          </tbody>
-        </table>
-      </div>
+          </TableBody>
+        </Table>
+      </main>
     </div>
   );
 };
