0c1c0b98b515bbe4f1067025b229e5f1506bdc7c
nhwnahid007 <nhwnahid@gmail.com>
2024-10-17 23:19:18 +0600

    added hello teacher in homepage

--- src/app/(withComonLayout)/page.js
+++ src/app/(withComonLayout)/page.js
@@ -4,11 +4,13 @@ import Footer from "@/components/Shared/Footer";
 import Feedback from "@/components/HomePage/Feedback";
 import Partnership from "@/components/HomePage/Partnership";
 import Faq from "@/components/HomePage/Faq";
+import HelloTeacher from "@/components/HomePage/HelloTeacher";
 
 export default function Home() {
   return (
     <div className="">
       <Banner />
+      <HelloTeacher></HelloTeacher>
       <Overview />
       <Feedback />
       <Partnership />
