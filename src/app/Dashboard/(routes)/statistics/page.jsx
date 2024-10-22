5d1913ead6416dbdedeb514291f9834aa64be4ad
musaakramsaleh <saleh01715972188@gmail.com>
2024-10-22 12:15:19 +0600

    another merge done

--- src/app/Dashboard/(routes)/statistics/page.jsx
+++ src/app/Dashboard/(routes)/statistics/page.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+'use client'
+import { getMarks } from '@/requests/get';
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
+    const [marks,setMarks] = useState([])
+    useEffect(() => {
+        const allmarks = async () => {
+          try {
+            const data = await getMarks('saleh01715972188@gmail.com');
+            setMarks(data);
+          } catch (error) {
+            console.log("Data fetching error", error);
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "Something went wrong while fetching leaderboard data!",
+              toast: true,
+            });
+          }
+        };
+        allmarks();
+      }, []);
+    console.log(marks)
     return (
         <div>
-          <p>Hello world</p>
+          <p>Total Exams attempted {marks.length}</p>
         </div>
     );
 };
