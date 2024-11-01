3098e90f32e5b8f93b9f5284e4aa655676ec1892
rafiul-razib <rafiul.rzb1@gmail.com>
2024-11-01 11:55:10 +0600

    quiz history pagination added

--- src/app/Dashboard/(routes)/quizHistory/page.jsx
+++ src/app/Dashboard/(routes)/quizHistory/page.jsx
@@ -20,6 +20,8 @@ const QuizHistory = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   const [history, setHistory] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(10);
 
   const router = useRouterHook();
 
@@ -50,16 +52,32 @@ const QuizHistory = () => {
     router.push(`/Dashboard/viewHistory/${id}`);
   };
 
+  const totalPages = Math.ceil(history.length / itemsPerPage);
+  const pageRange = 3;
+  const getVisiblePages = () => {
+    const startPage = Math.max(1, currentPage - pageRange);
+    const endPage = Math.min(totalPages, currentPage + pageRange);
+    return Array.from(
+      { length: endPage - startPage + 1 },
+      (_, i) => startPage + i
+    );
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="h-screen max-w-6xl mx-auto pt-20">
+    <div className="h-screen max-w-6xl mx-auto pt-20 px-4 relative overflow-auto">
       <h1 className="text-center text-3xl  font-extrabold  mb-8">
         Quiz History
       </h1>
-      <div>
-        <Table>
-          <TableCaption>A list of your attemots in Custom Quiz.</TableCaption>
-          <TableHeader>
+      <div className="overflow-hidden shadow-md sm:rounded-lg">
+        <Table className="w-full min-w-full table-fixed">
+          <TableCaption>A list of your attempts in Custom Quiz.</TableCaption>
+          <TableHeader className="bg-gray-100">
             <TableRow>
+              <TableHead className="w-[60px]">#</TableHead>
               <TableHead className="w-[200px]">Date</TableHead>
               <TableHead>Quiz On</TableHead>
               <TableHead>Examiner</TableHead>
@@ -68,26 +86,71 @@ const QuizHistory = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {history.map((item) => (
-              <TableRow key={item._id}>
-                <TableCell className="font-medium">
-                  {moment(item.date).format("MMMM Do YYYY")}
-                </TableCell>
-                <TableCell>{item.quizCategory}</TableCell>
-                <TableCell>{item.quizCreator}</TableCell>
-                <TableCell className="text-center">{item.marks}%</TableCell>
-                <TableCell className="text-right">
-                  <Button
-                    onClick={() => handleDetails(item._id)}
-                    className="bg-primary-color"
-                  >
-                    Details
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
+            {history
+              .slice(
+                (currentPage - 1) * itemsPerPage,
+                currentPage * itemsPerPage
+              )
+              .map((item, idx) => (
+                <TableRow
+                  className="hover:bg-gray-50 transition-colors duration-200"
+                  key={item._id}
+                >
+                  <TableCell className="font-medium">{idx + 1}</TableCell>
+                  <TableCell>
+                    {moment(item.date).format("MMMM Do YYYY")}
+                  </TableCell>
+                  <TableCell>{item.quizCategory}</TableCell>
+                  <TableCell>{item.quizCreator}</TableCell>
+                  <TableCell className="text-center font-bold">
+                    {item.marks}%
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Button
+                      onClick={() => handleDetails(item._id)}
+                      className="bg-primary-color"
+                    >
+                      Details
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
+      </div>
+      {/* Pagination */}
+      <div className="flex justify-center mt-4 space-x-2">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`px-4 py-2 rounded-md ${
+            currentPage === 1 ? "bg-gray-300" : "bg-primary text-white"
+          }`}
+        >
+          &#8592;
+        </button>
+
+        {getVisiblePages().map((page) => (
+          <button
+            key={page}
+            onClick={() => handlePageChange(page)}
+            className={`px-4 py-2 rounded-md ${
+              currentPage === page ? "bg-primary text-white" : "bg-gray-200"
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className={`px-4 py-2 rounded-md ${
+            currentPage === totalPages ? "bg-gray-300" : "bg-primary text-white"
+          }`}
+        >
+          &#8594;
+        </button>
       </div>
     </div>
   );
